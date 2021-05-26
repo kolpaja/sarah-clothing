55bfe0fd2e6cc4b648505ee921c35a55f36c6c75
kolpaja <79046681+kolpaja@users.noreply.github.com>
2021-05-26 18:01:28 +0200

    add shoppage

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import React from "react";
 import "./App.css";
 import { Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component"
 
-const hatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Route exact path="/" component={HomePage} />
-      <Route path="/hats" component={hatsPage} />
+      <Route path="/shop" component={ShopPage} />
     </div>
   );
 }
