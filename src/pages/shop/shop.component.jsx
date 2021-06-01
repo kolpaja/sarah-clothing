3bca2761b1109e2dc0a81c710cf9383ec0832c0e
kolpaja <79046681+kolpaja@users.noreply.github.com>
2021-06-01 13:30:52 +0200

    add shop collection route

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,18 @@
-import React from "react"
-import SHOP_DATA from "./shop.data.js"
+import React from "react";
 
-import CollectionPreview from "../../components/collection-preview/collection-preview.component"
+import { Route } from "react-router-dom";
 
+import CollectionOverview from "../../components/collection-overview/collection-overview.component";
+import CollectionPage from "../collection/collection.component";
 
-class ShopPage extends React.Component{
-    constructor(props){
-        super(props)
-
-    this.state={
-        collections: SHOP_DATA
-    }
-    }
-
-
-    render(){
-        const {collections} = this.state
-        return(<div className="shop-page">
-            {
-                collections.map(({id, ...otherCollectionProps}) =>
-                <CollectionPreview key={id} {...otherCollectionProps} />
-                )
-            }
-        </div>)
-    }
-}
+const ShopPage = ({ match }) => {
+  console.log(match.path);
+  return (
+    <div className="shop-page">
+      <Route exact path={`${match.path}`} component={CollectionOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+  );
+};
 
 export default ShopPage;
---
+++ src/redux/shop/shop.selectors.js
@@ -0,0 +1,25 @@
+import { createSelector } from "reselect";
+import memoize from "lodash.memoize";
+
+const selectShop = (state) => state.shop;
+
+const COLLECTION_ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5,
+};
+
+export const selectShopCollections = createSelector(
+  [selectShop],
+  (shop) => shop.collections
+);
+
+export const selectCollection = memoize((collectionUrlParam) =>
+  createSelector([selectShopCollections], (collections) =>
+    collections.find(
+      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+    )
+  )
+);
