0c4b680a4e9622919392d18c8117a15bc9233a7c
kolpaja <79046681+kolpaja@users.noreply.github.com>
2021-06-01 10:36:09 +0200

    add local storage with redux-persist to save the cartItems on page reload

--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </React.StrictMode>
   </Provider>,
