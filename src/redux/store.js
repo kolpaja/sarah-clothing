e90ec8a6ef7e101dfdbf24a816febc0cb8d0e58a
kolpaja <79046681+kolpaja@users.noreply.github.com>
2021-06-02 11:56:29 +0200

    remove logger form production, on the react-redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,7 @@ import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 
 const middlewares = [];
-if (proccess.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
