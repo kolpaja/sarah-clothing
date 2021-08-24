cbd5a3f3f5f823db6baa39032a5ba556fb6cbe9b
sokol paja <kolpaja28@gmail.com>
2021-08-24 09:50:10 +0200

    edited contanct page

--- client/src/pages/contact/contact.component.jsx
+++ client/src/pages/contact/contact.component.jsx
@@ -2,6 +2,13 @@ import React from "react";
 
 import "./contact.styles.scss";
 
-const ContactPage = () => <div>Contact page</div>;
+const ContactPage = () => (
+  <div>
+    Contact page...
+    <a href="https://gatsbyprojectmaster12650.gatsbyjs.io/">
+      Personal Portfolio
+    </a>
+  </div>
+);
 
 export default ContactPage;
